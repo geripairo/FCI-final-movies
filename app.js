555fcc30ec7123c3ee77ece59d923e9e6bee1872
Gerard Pairo <gerardpairo@hotmail.com>
2023-05-24 07:24:46 +0200

    initializing repo

---
+++ app.js
@@ -0,0 +1,4 @@
+import {movieDB} from './library.js'
+const {api_key, base_url, images_url} = movieDB
+
+console.log(`${base_url}${api_key}`)
---
+++ library.js
@@ -0,0 +1,52 @@
+export const movieDB = {
+    api_key: "?api_key=8114c981400cf1383e7886a00fb9f174",
+    base_url: "http://api.themoviedb.org/3/movie?",
+    images_url: "http://image.tmdb.org/t/p/",
+    timeout: 5000,
+    language: "en-US",
+}
+
+/* https://api.themoviedb.org/3/discover/movie?page=2&sort_by=popularity.desc&api_key=8114c981400cf1383e7886a00fb9f174
+
+https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&with_genres=horror&api_key=8114c981400cf1383e7886a00fb9f174 */
+
+/* MOVIE
+Action          28
+Adventure       12
+Animation       16
+Comedy          35
+Crime           80
+Documentary     99
+Drama           18
+Family          10751
+Fantasy         14
+History         36
+Horror          27
+Music           10402
+Mystery         9648
+Romance         10749
+Science Fiction 878
+TV Movie        10770
+Thriller        53
+War             10752
+Western         37
+
+
+
+TV SHOW
+Action & Adventure  10759
+Animation           16
+Comedy              35
+Crime               80
+Documentary         99
+Drama               18
+Family              10751
+Kids                10762
+Mystery             9648
+News                10763
+Reality             10764
+Sci-Fi & Fantasy    10765
+Soap                10766
+Talk                10767
+War & Politics      10768
+Western             37 */
