109c75881a6cb5bfb4052eeafac9d666b7010d5f
Gerard Pairo <gerardpairo@hotmail.com>
2023-05-25 20:17:17 +0200

    creating movie details

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import {movieDB} from './library.js'
-const {api_key, base_url, images_url} = movieDB
-const main = document.querySelector('main')
+const {API_KEY, BASE_URL, IMAGES_URL, DISCOVER_URL} = movieDB
+
+import {genres} from './library.js'
 
 
-// console.log(`${base_url}${api_key}`)
-getMovies();
-function getMovies (){
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=8114c981400cf1383e7886a00fb9f174&language=en-US&page=1&sort_by=popularity.desc')
+const cardsSection = document.querySelector('#movie-cards-wrapper')
+const filmDetailSection = document.querySelector('#movie-details-wrapper');
+const header = document.querySelector('.header-search')
+let searchBar = document.querySelector('#search-bar')
+
+let page = 1
+let filmID = 0
+const discoverUrl = `${DISCOVER_URL}${API_KEY}&language=en-US&page=${page}&sort_by=popularity.desc`
+const searchUrl = `${BASE_URL}/search/movie${API_KEY}`
+
+// CONSEGUIR EL DATA DE LAS PELICULAS POR POPULARIDAD
+getMovies(`${DISCOVER_URL}${API_KEY}&language=en-US&page=${page}&sort_by=popularity.desc`);
+function getMovies (url){
+  fetch(url)
   .then(response => response.json())
   .then(data => {
     console.log(data)
@@ -15,29 +26,104 @@ function getMovies (){
   .catch(err => console.log(err));
 }
 
+// IMPRIMIR CARTAS DE LAS PELÍCULAS EN LA PÁGINA
 function showMovies(data){
-  main.innerHTML = '';
+  cardsSection.innerHTML = '';
   data.forEach(movie => {
-    const {title, poster_path, vote_average, overview} = movie;
-    const movieItem = document.createElement('div');
-    movieItem.classList.add('movie');
-    movieItem.innerHTML = `
-    <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
-
-   <div class="movie-info">
-       <h3>${title}</h3>
-       <span class="${getColor(vote_average)}">${vote_average}</span>
-   </div>
-
-   <div class="overview">
-
-       <h3>Overview</h3>
-       ${overview}
-       <br/> 
-       <button class="know-more" id="${id}">Know More</button
-   </div>
-
+    const {title, poster_path, vote_average, overview, genre_ids, id} = movie;
+    
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie');
+    movieCard.innerHTML = `
+    <p class="filmID">${id}</p>
+    <img src="${IMAGES_URL}/w500/${poster_path}" alt="${title} movie image">
+    <div class="card__overlay">
+      <div class="movie-info overlay_text">
+        <h3 class="movie-title">${title}</h3>        
+        <span class="movie-rate ${getColors(vote_average)}">${vote_average.toFixed(1)}</span>
+        <span class="movie-genre">| ${getGenres(genre_ids)}</span>
+      </div>
+    </div>
 `
-main.appendCChild(movieItem);
+  
+  cardsSection.appendChild(movieCard);
+  movieCard.addEventListener('click', (e) => {
+    let target = e.currentTarget;
+    let focus = target.children[0].innerText
+    filmID = focus;
+
+    filmDetail(filmID)
   })
+})
+}
+
+function getColors(rate){  
+  if(rate <= 4){return "red-rate"}      
+  if(rate > 4 && rate <= 6.5) {return "orange-rate"}
+  else return "green-rate"
+}
+
+function getGenres(genre){
+  let filmGenre = ""
+  for (let item of genres) {
+    if(item.id == genre[0]) filmGenre = `${item.name}`
+    if (item.id == genre[1]) filmGenre += `-${item.name}`
+    else filmGenre += ""
+    if (item.id == genre[2]) filmGenre += `-${item.name}`
+    else filmGenre += ""
+  } 
+  return filmGenre
+}
+
+// FUNCIONES PARA PASAR DE PÁGINA
+function nextPage(){
+  page+=1
+}
+
+function prevPage(){
+  if(page > 1){
+    page-=1
+  } else page+=0  
+}
+
+const nextBtn = document.querySelector('#next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+
+nextBtn.addEventListener('click', () => {
+  nextPage();
+  getMovies(`${DISCOVER_URL}${API_KEY}&language=en-US&page=${page}&sort_by=popularity.desc`);
+});
+prevBtn.addEventListener('click', () => {
+  prevPage();
+  getMovies(`${DISCOVER_URL}${API_KEY}&language=en-US&page=${page}&sort_by=popularity.desc`);
+});
+
+// FILTRAR PELICULAS POR EL BUSCADOR
+
+searchBar.addEventListener("keydown", (e) => {
+  if (e.key == "Enter"){
+    const searchValue = searchBar.value;
+    if(searchValue){
+      getMovies(`${searchUrl}&query=${searchValue}`)
+    }
+   }
+  });
+
+// MOSTRAR DETALLES PELÍCULA
+function filmDetail(id) {
+  fetch(`${BASE_URL}movie/${id}${API_KEY}&append_to_response=videos,images,genres,credits`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    const {backdrop_path} = data;
+    header.classList.add('header-active')
+    header.style.background = `url(${IMAGES_URL}/original${backdrop_path}) center / cover`
+  } )
+  .catch(err => console.log(err));  
+  
+  cardsSection.style.display = "none"
+  filmDetailSection.style.display = "block"
+  document.querySelector('.page-buttons').style.display = "none"
+  
+  
 }
--- library.js
+++ library.js
@@ -2,9 +2,92 @@ export const movieDB = {
     API_KEY: "?api_key=8114c981400cf1383e7886a00fb9f174",
     BASE_URL: "http://api.themoviedb.org/3/",
     IMAGES_URL: "http://image.tmdb.org/t/p/",
+    DISCOVER_URL: "http://api.themoviedb.org/3/discover/movie",
     timeout: 5000,
     language: "en-US",
 }
+
+export const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+  
+
             // Buscar pelicula con search
 // https://api.themoviedb.org/3/search/movie?api_key=8114c981400cf1383e7886a00fb9f174&query=Jack+Reacher
             // buscar los detalles de esa pelicula con su ID
@@ -12,30 +95,11 @@ export const movieDB = {
             // Buscar con videos y imágenes
 // https://api.themoviedb.org/3/movie/343611?api_key=8114c981400cf1383e7886a00fb9f174&append_to_response=videos,images,genres
 
-/* MOVIE
-Action          28
-Adventure       12
-Animation       16
-Comedy          35
-Crime           80
-Documentary     99
-Drama           18
-Family          10751
-Fantasy         14
-History         36
-Horror          27
-Music           10402
-Mystery         9648
-Romance         10749
-Science Fiction 878
-TV Movie        10770
-Thriller        53
-War             10752
-Western         37
 
 
 
-TV SHOW
+
+/* TV SHOW
 Action & Adventure  10759
 Animation           16
 Comedy              35
@@ -51,4 +115,4 @@ Sci-Fi & Fantasy    10765
 Soap                10766
 Talk                10767
 War & Politics      10768
-Western             37 */
+Western             37  */
